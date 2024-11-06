574b67a3ad38f0392e8b31828154fa2a7db34a92
Luiz Bernardo <luiz.bernardo.ext@ipiranga.ipiranga>
2024-11-06 19:41:30 -0300

    c12: feat - exercicio zip

--- temas/zip.py
+++ temas/zip.py
@@ -1,8 +1,24 @@
-'''nums = '1234' 
+nums = '1234' 
 letters = ['a','b','c','d']
 names =['John','Eric','Michael','Graham','Joe']
-combo = list(zip(nums,letters,names))
+# combo = list(zip(nums,letters,names))
+# print(combo)
+# num,let,nam =zip(*combo)
+# print(num,let,nam)
+nums = list(nums)
+#print(list(nums))
+combo = list(zip(nums,names))
 print(combo)
-num,let,nam =zip(*combo)
-print(num,let,nam)
-'''
+
+combo = list(zip(nums,names,letters))
+print(combo)
+
+
+# dicionarios 
+combo = dict(zip(nums, (*letters,*names)))
+print(combo, ' teste')
+
+combo = {num: (letters,names) for num, letters, names in zip(nums,letters,names )}
+print(combo)
+#combo = dict(zip(nums,names,letters))
+print(combo)
